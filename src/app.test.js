ef2832342863d22cb62d375036bac172b95a29d5
Martin Cavoj <martin.cavoj@renuo.ch>
2018-05-08 18:20:27 +0200

    setup tests

--- src/app.test.js
+++ src/app.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import mockFetch from 'jest-fetch-mock';
+
+describe('App', () => {
+  beforeEach(() => {
+    mockFetch.mockResponse('[]');
+  });
+
+  it('matches the snapshot', () => {
+    const tree = renderer.create(<App/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });

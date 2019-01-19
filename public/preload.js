c77bd4cab63e5b846fde67c4c040683ce1fa5c68
Elliot Derhay <e.j.derhay@gmail.com>
2019-01-18 21:26:04 -0500

    Read hotel config and attach port to `window` object
    This is done in `preload.js` to attempt to give the front-end access to the port value.

--- public/preload.js
+++ public/preload.js
@@ -1,2 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const homedir = require('os').homedir();
 const { shell, Tray } = require('electron');
+
+// Read Hotel config and store port number
+const hotelConfigPath = path.join(homedir, '.hotel', 'conf.json');
+const hotelPort = JSON.parse(fs.readFileSync(hotelConfigPath)).port;
+
+// Attach electron shell and hotel port to window object
 window.shell = shell;
+window.hotelPort = hotelPort ? hotelPort.toString() : '2000';

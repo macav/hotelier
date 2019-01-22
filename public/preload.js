c1377e0a670d8e1915e1da8323cc0c6a1f646a41
Elliot Derhay <e.j.derhay@gmail.com>
2019-01-22 12:33:54 -0500

    Attach ipcRenderer to window in preload.js
    I'm told this is the correct way to give the renderer process access to the ipcRenderer object. Hoping it works out that way.

--- public/preload.js
+++ public/preload.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const homedir = require('os').homedir();
 // eslint-disable-next-line no-unused-vars
-const { shell, Tray } = require('electron');
+const { shell, Tray, ipcRenderer } = require('electron');
 
 // Read Hotel config and store port number
 const hotelConfigPath = path.join(homedir, '.hotel', 'conf.json');
 const hotelPort = JSON.parse(fs.readFileSync(hotelConfigPath)).port;
 
-// Attach electron shell and hotel port to window object
+// Attach electron shell, ipcRenderer, and hotelPort to window object
 window.shell = shell;
 window.hotelPort = hotelPort ? hotelPort.toString() : '2000';
+window.ipcRenderer = ipcRenderer;
